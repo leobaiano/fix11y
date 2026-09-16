bade829c966926deed8b6d20c183f53995df15b6
Leo Baiano <ljunior2005@gmail.com>
2026-09-16 12:54:35 -0300

    fix: adiciona assetPrefix e condicional de ambiente no next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/fix11y',
+  basePath: isProd ? '/fix11y' : '',
+  assetPrefix: isProd ? '/fix11y/' : '',
   images: {
     unoptimized: true,
   },
